6ab68b805e0d7902ee9f022199997a444d6240f7
Antonio David Gellida Lavara <adgellida@gmail.com>
2017-04-27 21:28:22 +0200

    "Updating"

--- 001.installChocolatey_cmd.bat
+++ 001.installChocolatey_cmd.bat
@@ -1,10 +1,12 @@
 @echo off
 
+rem #### 001.installChocolatey
+
 rem #### install chocolatey - http://community.spiceworks.com/scripts/show/2602-chocolatey-nuget-install-command-line-method
 rem #### All Windows versions support commenting a line and uncommenting another.
 
-@powershell Write-Host "Installing Chocolatey, Nuget Package Explorer and ChocolateyGUI... Please wait"
-@powershell Start-Sleep -s 3
+rem @powershell Write-Host "Installing Chocolatey, Nuget Package Explorer and ChocolateyGUI... Please wait"
+rem @powershell Start-Sleep -s 3
 
 rem #### for Windows Vista and upper (7, 8, 8.1, 10, etc) 
 rem @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
