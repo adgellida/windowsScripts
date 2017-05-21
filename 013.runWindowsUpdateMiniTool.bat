d5a2b5618a568543fe632d0c75f56e814d854b6f
Antonio David Gellida Lavara <adgellida@gmail.com>
2017-05-21 23:41:56 +0200

    "Updating"

--- 013.runWindowsUpdateMiniTool.bat
+++ 013.runWindowsUpdateMiniTool.bat
@@ -5,7 +5,7 @@ rem #### 013.runWindosUpdateMiniTool
 cd..
 cd..
 cd downloads
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "$source = 'http://www.adgellida.byethost7.com/files/wumt.zip'; $destination = 'wumt.zip'; Invoke-WebRequest $source -OutFile $destination;
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive wumt.zip -DestinationPath wumt; $file = 'wumt/wumt_x64.exe'; $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "$source = 'https://www.ogzdq5-user.freehosting.host/files/wumt.zip'; $destination = 'wumt.zip'; Invoke-WebRequest $source -OutFile $destination;
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive wumt.zip -DestinationPath wumt; $file = 'wumt\wumt_x64.exe'; $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;"
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "Remove-Item wumt.zip";
 rem pause
