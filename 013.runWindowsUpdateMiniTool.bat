30078863efd4a0efeec38a9b1417fbc57404fa8c
Antonio David Gellida Lavara <adgellida@gmail.com>
2017-05-21 21:20:25 +0200

    "Updating"

--- 013.runWindowsUpdateMiniTool.bat
+++ 013.runWindowsUpdateMiniTool.bat
@@ -5,7 +5,7 @@ rem #### 013.runWindosUpdateMiniTool
 cd..
 cd..
 cd downloads
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "$source = 'https://doc-0o-88-docs.googleusercontent.com/docs/securesc/11c6obe2cu2h77a0tdds5vm2b0smgsog/jlk86sto3lfiq1pagkredvvhsv4mu91u/1495267200000/02572933763160398265/16071247937085431459/0BwJH2CazcjsINFZFc1pVdk9mNHM?e=download&nonce=p29mmovmg73k4&user=16071247937085431459&hash=a5e6sccniu7ogfqa016gme6jktq838k4'; $destination = 'wumt.zip'; Invoke-WebRequest $source -OutFile $destination;
-rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive v20.12.2016.zip -DestinationPath v20.12.2016; $file = 'v20.12.2016/wumt_x64.exe'; $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;"
-rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "Remove-Item wumt_v20.12.2016.zip";
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "$source = 'www.adgellida.byethost7.com/files/wumt.zip'; $destination = 'wumt.zip'; Invoke-WebRequest $source -OutFile $destination;
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive wumt.zip -DestinationPath wumt; $file = 'wumt/wumt_x64.exe'; $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Remove-Item wumt.zip";
 rem pause
