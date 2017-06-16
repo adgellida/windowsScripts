557580e408e7d778b4bcf0a60f8a56f1f4daeb46
Antonio David Gellida Lavara <adgellida@gmail.com>
2017-06-16 17:11:13 +0200

    "Updating"

--- 022.downloadLatestWindowsScripts.ps1
+++ 022.downloadLatestWindowsScripts.ps1
@@ -1,7 +1,5 @@
-@echo off
+#### 022.downloadLatestWindowsScripts
 
-rem #### 022.downloadLatestWindowsScripts
-
-echo 'Downloading updated scripts. Please wait... Stage 1/3'; $down = New-Object System.Net.WebClient; $url = 'https://github.com/adgellida/windowsScripts/archive/master.zip'; $file = 'master.zip'; $down.DownloadFile($url,$file);
-echo 'Cleaning old scripts. Please wait... Stage 2/3'; Remove-Item scripts -recurse;"
-echo 'Preparing scripts to be used. Please wait... Stage 3/3'; Expand-Archive master.zip -DestinationPath scripts; New-Item -ItemType Directory -Force -Path downloads; Remove-Item master.zip;
+echo Downloading updated scripts. Please wait... Stage 1/3; $down = New-Object System.Net.WebClient; $url = 'https://github.com/adgellida/windowsScripts/archive/master.zip'; $file = 'master.zip'; $down.DownloadFile($url,$file);
+echo Cleaning old scripts. Please wait... Stage 2/3; Remove-Item scripts -recurse;
+echo Preparing scripts to be used. Please wait... Stage 3/3; Expand-Archive master.zip -DestinationPath scripts; New-Item -ItemType Directory -Force -Path downloads; Remove-Item master.zip;
