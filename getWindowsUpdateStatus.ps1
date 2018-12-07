85cdf236ad964b2f3efef889c2374fdb159a0588
Antonio David Gellida Lavara <adgellida@gmail.com>
2018-12-07 01:31:25 +0100

    "Updating"

--- getWindowsUpdateStatus.ps1
+++ getWindowsUpdateStatus.ps1
@@ -3,8 +3,10 @@ $serviceStatus = (get-service "$serviceName").Status;
 
 if ($serviceStatus -eq "Running") {
     echo "Service is Running";
+	echo "Running" >> "/status/updateStatus.txt";
 }
 else {
     #Could be Stopped, Stopping, Paused, or even Starting...
     echo "Service is $serviceStatus";
+	echo "Not running" >> "/status/updateStatus.txt";
 }
