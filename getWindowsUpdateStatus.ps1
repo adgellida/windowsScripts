7dc8ee3400192204eaf21a6bdebc62e329630bbe
Antonio David Gellida Lavara <adgellida@gmail.com>
2018-12-07 01:04:13 +0100

    "Updating"

---
+++ getWindowsUpdateStatus.ps1
@@ -0,0 +1,10 @@
+$serviceName = "wuauserv";
+$serviceStatus = (get-service "$serviceName").Status;
+
+if ($serviceStatus -eq "Running") {
+    echo "Service is Running";
+}
+else {
+    #Could be Stopped, Stopping, Paused, or even Starting...
+    echo "Service is $serviceStatus";
+}
