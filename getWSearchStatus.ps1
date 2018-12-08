239333ac9df2968e41f16345fe6bffcb3d0efdab
Antonio David Gellida Lavara <adgellida@gmail.com>
2018-12-08 12:14:08 +0100

    "Updating"

--- getWSearchStatus.ps1
+++ getWSearchStatus.ps1
@@ -1,12 +1,13 @@
 $serviceName = "wsearch";
+$serviceShowName = "Windows Search";
 $serviceStatus = (get-service "$serviceName").Status;
 
 if ($serviceStatus -eq "Running") {
-    echo "Service is Running";
+    echo "serviceShowName is Running";
 	echo "Running" > "WSearchStatus.txt";
 }
 else {
     #Could be Stopped, Stopping, Paused, or even Starting...
-    echo "Service is $serviceStatus";
+    echo "serviceShowName is $serviceStatus";
 	echo "Not running" > "WSearchStatus.txt";
 }
--- getWuauservStatus.ps1
+++ getWuauservStatus.ps1
@@ -1,12 +1,13 @@
 $serviceName = "wuauserv";
+$serviceShowName = "Windows Update";
 $serviceStatus = (get-service "$serviceName").Status;
 
 if ($serviceStatus -eq "Running") {
-    echo "Service is Running";
+    echo "$serviceShowName is Running";
 	echo "Running" > "WuauservStatus.txt";
 }
 else {
     #Could be Stopped, Stopping, Paused, or even Starting...
-    echo "Service is $serviceStatus";
+    echo "$serviceShowName is $serviceStatus";
 	echo "Not running" > "WuauservStatus.txt";
 }
